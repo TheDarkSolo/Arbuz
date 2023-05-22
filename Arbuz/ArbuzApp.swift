d14e0a94d3a7bbae89b89056fb1e204905c8480e
Tore <anurtore01@gmail.com>
2023-05-23 05:25:45 +0600

    Initial Commit

---
+++ Arbuz/ArbuzApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArbuzApp.swift
+//  Arbuz
+//
+//  Created by Tore Amangeldy on 5/23/23.
+//
+
+import SwiftUI
+
+@main
+struct ArbuzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
